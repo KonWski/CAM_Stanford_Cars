58e4e24f1c55e77e19f898b5676a1a7ad1242a74
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-11-14 23:02:22 +0100

    transforms

--- transforms.py
+++ transforms.py
@@ -3,10 +3,10 @@ from torchvision import transforms
 transform_predict = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((256, 256)),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.ColorJitter(brightness=(0.5, 1), hue=.3),
             transforms.RandomHorizontalFlip(),
             transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
-            transforms.ColorJitter(brightness=(0.5, 1), hue=.3)
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
 
 transform_test = transforms.Compose([
