28e20a83f74df4cdc86179623a3d3fed9cf20141
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-11-14 23:29:57 +0100

    grayscale

--- transforms.py
+++ transforms.py
@@ -3,7 +3,7 @@ from torchvision import transforms
 transform_predict = transforms.Compose([
             transforms.ToTensor(),
             transforms.Resize((256, 256)),
-            transforms.ColorJitter(brightness=(0.5, 1), hue=.3),
+            transforms.RandomGrayscale(0.3),
             transforms.RandomHorizontalFlip(),
             transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
