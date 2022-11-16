91aba60739c9091ee868a3f30fc4b6b1798c2b69
Konrad Witkowski <k.l.p.witkowski@gmail.com>
2022-11-16 23:12:38 +0100

    augmentation

--- transforms.py
+++ transforms.py
@@ -5,7 +5,6 @@ transform_predict = transforms.Compose([
             transforms.Resize((256, 256)),
             transforms.RandomGrayscale(0.3),
             transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(hue=0.3),
             transforms.RandomPerspective(distortion_scale=0.6, p=1.0),
             transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
